a1c16416960a040db091e1adaf237e90fcf69def
hanbin <hanbin8269@gmail.com>
2020-01-16 17:01:50 +0900

    [CREATE]Controller

--- src/models/index.js
+++ src/models/index.js
@@ -1,1 +1,17 @@
 import Sequelize from 'sequelize';
+import path from 'path';
+
+import { User } from './User';
+
+const config = require(path.join(__dirname,'..','config','config.json'))['development'];
+
+const sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+)
+
+const user = User(sequelize, Sequelize);
+
+export { sequelize, Sequelize, user };
